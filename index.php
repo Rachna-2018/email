539b84c126044efe5a4f809a6bc15a34d4d2c720
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-20 12:07:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == 'POST')
 {
-	//use PHPMailer\PHPMailer\PHPMailer;
+	/*//use PHPMailer\PHPMailer\PHPMailer;
 	//use PHPMailer\PHPMailer\Exception;
 
 	//Load Composer's autoloader
@@ -44,7 +44,41 @@ if($method == 'POST')
 	$response = new \stdClass();
     	$response->fulfillmentText = $speech;
     	$response->source = "webhook";
-	echo json_encode($response);
+	echo json_encode($response);*/
+	
+	$smtpUsername = 'rachnarke@gmail.com';
+	$smtpPassword =	'av!k2010';
+	$emailFrom = 'rachnarke@gmail.com';
+	$emailFromName = 'Rachna Bhatnagar';
+	$emailToName = 'Rachna Bhatnagar';
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	$Email = $json->queryResult->parameters->Email;	 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'PHPMailer/src/Exception.php';
+	require 'PHPMailer/src/PHPMailer.php';
+	require 'PHPMailer/src/SMTP.php';
+
+	$mail = new PHPMailer;
+	$mail->isSMTP(); 
+	$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+	$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+	$mail->Port = 587; // TLS only
+	$mail->SMTPSecure = 'tls'; // ssl is depracated
+	$mail->SMTPAuth = true;
+	$mail->Username = $smtpUsername;
+	$mail->Password = $smtpPassword;
+	$mail->setFrom($emailFrom, $emailFromName);
+	$mail->addAddress($Email, $emailToName);
+	$mail->Subject = 'PHPMailer GMail SMTP test';
+	$mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+	$mail->AltBody = 'HTML messaging not supported';
+	// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+
+	if(!$mail->send()){ echo "Mailer Error: " . $mail->ErrorInfo;}
+	else{ echo "Message sent!";}
 
 }
 else
@@ -52,5 +86,7 @@ else
 	echo "Method not allowed";
 }
 
+	
+	
 ?>
 
