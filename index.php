e0e2431fb55ff2377d65edade22b711f338dd048
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-20 12:28:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -57,9 +57,9 @@ if($method == 'POST')
 	//use PHPMailer\PHPMailer\PHPMailer;
 	//use PHPMailer\PHPMailer\Exception;
 
-	require 'PHPMailer/src/Exception.php';
-	require 'PHPMailer/src/PHPMailer.php';
-	require 'PHPMailer/src/SMTP.php';
+	require '../src/Exception.php';
+	require '../src/PHPMailer.php';
+	require '../src/SMTP.php';
 
 	$mail = new PHPMailer;
 	$mail->isSMTP(); 
