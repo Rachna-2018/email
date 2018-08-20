0565f1636ee8b542b9b32f06173f941682c09894
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-20 10:41:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == 'POST')
 {
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
+	//use PHPMailer\PHPMailer\PHPMailer;
+	//use PHPMailer\PHPMailer\Exception;
 
 	//Load Composer's autoloader
-	require '../vendor/autoload.php';
+	//require '../vendor/autoload.php';
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	    
