ab72fffe05dea5ee5b1ee1e9893fd40effc4f1f8
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-20 12:40:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -46,22 +46,23 @@ if($method == 'POST')
     	$response->source = "webhook";
 	echo json_encode($response);*/
 	
-	$smtpUsername = 'rachnarke@gmail.com';
-	$smtpPassword =	'avik17jan';
-	$emailFrom = 'rachnarke@gmail.com';
-	$emailFromName = 'Rachna Bhatnagar';
-	$emailToName = 'Rachna Bhatnagar';
+	
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$Email = $json->queryResult->parameters->Email;	 
-	//use PHPMailer\PHPMailer\PHPMailer;
-	//use PHPMailer\PHPMailer\Exception;
+	$smtpUsername = "rachnarke@gmail.com";
+	$smtpPassword =	"avik17jan";
+	$emailFrom = "rachnarke@gmail.com";
+	$emailFromName = "Rachna Bhatnagar";
+	$emailToName = "Rachna Bhatnagar";
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
-	require '../src/Exception.php';
-	require '../src/PHPMailer.php';
-	require '../src/SMTP.php';
+	require '/src/Exception.php';
+	require "/src/PHPMailer.php";
+	require "/src/SMTP.php";
 
-	$mail = new PHPMailer;
+	$mail = new PHPMailer();
 	$mail->isSMTP(); 
 	$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
 	$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
