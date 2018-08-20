b345b0d87be8a3c4fd6f866336ef292fc79e0a78
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-20 14:31:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,13 @@ if($method == 'POST')
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$Email = $json->queryResult->parameters->Email;	 
-	$smtpUsername = 'rachnarke@gmail.com';
-	$smtpPassword =	'avik17jan';
-	$emailFrom = 'rachnarke@gmail.com';
-	$emailFromName = 'Rachna Bhatnagar';
-	$emailToName = 'Rachna Bhatnagar';
+	$smtpUsername = "rachnarke@gmail.com";
+	$smtpPassword =	"avik17jan";
+	$emailFrom = "rachnarke@gmail.com";
+	$emailFromName = "Rachna Bhatnagar";
+	$emailToName = "Rachna Bhatnagar";
+	
+	echo $Email; echo $smtpUsername; echo $smtpPassword; echo $emailFrom; echo $emailFromName; echo $emailToName;
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\Exception;
 
@@ -30,6 +32,7 @@ if($method == 'POST')
 	require_once('class.smtp.php');
 	$mail = new PHPMailer();
 	$mail->isSMTP(); 
+	echo $mail->isSMTP(); 
 	//$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
 	$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
 	$mail->Port = 587; // TLS only
